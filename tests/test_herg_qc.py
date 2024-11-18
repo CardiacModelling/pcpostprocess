14fd05224466fc18dd3c0082df4b0ce5dd985043
Kwabena N Amponsah <kwabenantim@yahoo.com>
2024-11-18 15:40:21 +0000

    #21 Add test_qc4

--- tests/test_herg_qc.py
+++ tests/test_herg_qc.py
@@ -92,6 +92,8 @@ class TestHergQC(unittest.TestCase):
                 f"({rseal}, {cm}, {rseries})",
             )
 
+        # TODO: Test on select data
+
     def test_qc2(self):
         plot_dir = os.path.join(self.output_dir, "test_qc2")
         if not os.path.exists(plot_dir):
@@ -117,6 +119,8 @@ class TestHergQC(unittest.TestCase):
         recording = np.asarray([0, 1] * 1000)  # snr = 1.0
         result = hergqc.qc2(recording)
         self.assertFalse(result[0], f"({result[1]})")
+
+        # TODO: Test on select data
 
     def test_qc3(self):
         plot_dir = os.path.join(self.output_dir, "test_qc3")
@@ -144,13 +148,44 @@ class TestHergQC(unittest.TestCase):
             result = hergqc.qc3(recording0, recording1)
             self.assertEqual(result[0], expected, f"({result[1]})")
 
+        # TODO: Test on select data
+
     def test_qc4(self):
-        pass
+        def passed(result):
+            return all([x for x, _ in result])
+
+        plot_dir = os.path.join(self.output_dir, "test_qc1")
+        if not os.path.exists(plot_dir):
+            os.makedirs(plot_dir)
+
+        hergqc = hERGQC(
+            sampling_rate=self.sampling_rate, plot_dir=plot_dir, voltage=self.voltage
+        )
+
+        # qc4 checks that rseal, cm, rseries are similar before/after E-4031 change
+        test_matrix = [
+            [([1, 1], [1, 1], [1, 1]), True],
+            [([1, 2], [1, 2], [1, 2]), True],
+            [([1, 3], [1, 3], [1, 3]), True],
+            [([1, 4], [1, 4], [1, 4]), False],
+            [([1, 5], [1, 5], [1, 5]), False],
+        ]
+
+        for (rseals, cms, rseriess), expected in test_matrix:
+            self.assertEqual(
+                passed(hergqc.qc4(rseals, cms, rseriess)),
+                expected,
+                f"({rseals}, {cms}, {rseriess})",
+            )
+
+        # TODO: Test on select data
 
     def test_qc5(self):
+        # TODO: Test on select data
         pass
 
     def test_qc6(self):
+        # TODO: Test on select data
         pass
 
     def test_run_qc(self):
