2d54cf88253333c44f3ea5cb1b54908174493c28
hilaryh <17692590+hilaryh@users.noreply.github.com>
2024-11-19 15:42:22 +0000

    Flake8 issues 2

--- pcpostprocess/scripts/export_staircase_data.py
+++ pcpostprocess/scripts/export_staircase_data.py
@@ -21,7 +21,7 @@ from pcpostprocess.detect_ramp_bounds import detect_ramp_bounds
 from pcpostprocess.hergQC import hERGQC
 from pcpostprocess.infer_reversal import infer_reversal_potential
 from pcpostprocess.subtraction_plots import do_subtraction_plot
-from pcpostprocess.leak_correct import fit_linear_leak, get_leak_corrected
+from pcpostprocess.leak_correct import fit_linear_leak
 
 pool_kws = {'maxtasksperchild': 1}
 
