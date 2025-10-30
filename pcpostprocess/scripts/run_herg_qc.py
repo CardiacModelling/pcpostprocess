a72885d7f042a2bd3dd046cc8f3f0e525ad2ab81
Frankie Patten-Elliot <frankie.patten-elliott@nottingham.ac.uk>
2025-10-30 14:35:47 +0000

    Make default plotsize bigger

--- pcpostprocess/scripts/run_herg_qc.py
+++ pcpostprocess/scripts/run_herg_qc.py
@@ -51,7 +51,7 @@ def main():
     parser.add_argument('--reversal_spread_threshold', type=float, default=10)
     parser.add_argument('--export_failed', action='store_true')
     parser.add_argument('--selection_file')
-    parser.add_argument('--figsize', nargs=2, type=int, default=[5, 8])
+    parser.add_argument('--figsize', nargs=2, type=int, default=[16, 18])
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--log_level', default='INFO')
     parser.add_argument('--Erev', default=-90.71, type=float)
