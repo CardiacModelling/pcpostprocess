9d22edbe83e0e9609545915b86586d9652ed62b9
Michael Clerx <michael.clerx@nottingham.ac.uk>
2025-11-05 10:46:26 +0000

    Updated run_herg_qc to not use unnecessary n_sweeps constructor arg

--- pcpostprocess/scripts/run_herg_qc.py
+++ pcpostprocess/scripts/run_herg_qc.py
@@ -693,8 +693,7 @@ def extract_protocol(readname, savename, time_strs, selected_wells, args):
                 os.makedirs(plot_dir)
 
             hergqc = hERGQC(sampling_rate=before_trace.sampling_rate,
-                            plot_dir=plot_dir,
-                            n_sweeps=before_trace.NofSweeps)
+                            plot_dir=plot_dir)
 
             times = before_trace.get_times()
             voltage = before_trace.get_voltage()
