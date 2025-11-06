2f3913ac49d7e79321603a105fae4bf022faf55d
Joseph G. Shuttleworth <joey.shuttleworth@notitngham.ac.uk>
2025-11-06 11:18:58 +0000

    Update ramp bound function and docstring

--- pcpostprocess/detect_ramp_bounds.py
+++ pcpostprocess/detect_ramp_bounds.py
@@ -1,23 +1,23 @@
 import numpy as np
 
 
-def detect_ramp_bounds(times, voltage_sections, ramp_no=0):
+def detect_ramp_bounds(times, voltage_sections, ramp_index=0):
     """
-    Extract the the times at the start and end of the nth ramp in the protocol.
+    Extract the timepoint indices at the start and end of the nth ramp in the protocol.
 
     @param times: np.array containing the time at which each sample was taken
     @param voltage_sections 2d np.array where each row describes a segment of the protocol: (tstart, tend, vstart, end)
-    @param ramp_no: the index of the ramp to select. Defaults to 0 - the first ramp
+    @param ramp_index: the index of the ramp to select. Defaults to 0 - the first ramp
 
-    @returns tstart, tend: the start and end times for the ramp_no+1^nth ramp
+    @returns istart, iend: the start and end timepoint indices for the specified ramp
     """
 
     ramps = [(tstart, tend, vstart, vend) for tstart, tend, vstart, vend
              in voltage_sections if vstart != vend]
     try:
-        ramp = ramps[ramp_no]
+        ramp = ramps[ramp_index]
     except IndexError:
-        print(f"Requested {ramp_no+1}th ramp (ramp_no={ramp_no}),"
+        print(f"Requested {ramp_index+1}th ramp (ramp_index={ramp_index}),"
               " but there are only {len(ramps)} ramps")
 
     tstart, tend = ramp[:2]
