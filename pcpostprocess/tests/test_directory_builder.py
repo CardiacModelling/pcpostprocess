a4cef475b7eae28753e2433ce031da536494848f
Michael Clerx <michael.clerx@nottingham.ac.uk>
2025-11-12 18:54:14 +0000

    Simplified directory builder tests

--- pcpostprocess/tests/test_directory_builder.py
+++ pcpostprocess/tests/test_directory_builder.py
@@ -3,57 +3,39 @@ import os
 import re
 import tempfile
 import unittest
-from contextlib import ContextDecorator
-from unittest.mock import patch
 
 from pcpostprocess import directory_builder
 
 
-class temp_cwd(ContextDecorator):
-    def __enter__(self):
-        self.old = os.getcwd()
-        self.tmpdir = tempfile.TemporaryDirectory()
-        os.chdir(self.tmpdir.name)
-        return self.tmpdir
-
-    def __exit__(self, *exc):
-        os.chdir(self.old)
-        self.tmpdir.cleanup()
-        return False
+store_output = False
 
 
-def make_info_dict(lines):
-    return {l.split(": ", 1)[0]: l.split(": ", 1)[1]
-            for l in lines
-            if len(l.split(": ", 1)) == 2}
+def read_info_dict(path):
+    """ Reads a pcpostproces_info.txt and returns it as a dict. """
+    with open(path, 'r') as f:
+        items = [line.strip().split(': ') for line in f.readlines()]
+        return dict(line for line in items if len(line) == 2)
 
 
 class TestDirectoryBuilder(unittest.TestCase):
-    def test_directory_with_git(self):
-        test_dir = directory_builder.setup_output_directory("test_output",
-                                                            self.__class__.__name__)
+    """ Tests the DirectoryBuilder class. """
 
-        # Check that git commit is there
-        with open(os.path.join(test_dir, "pcpostprocess_info.txt"), "r") as fin:
-            info_file_contents = [l.strip() for l in fin.readlines()]
+    def test_directory_builder(self):
+        # Test that a pcpostprocess_info is written with a real git commit
 
-        info_dict = make_info_dict(info_file_contents)
+        with tempfile.TemporaryDirectory() as d:
+            if store_output:    # pragma: no cover
+                d = 'test_output'
+            path = directory_builder.setup_output_directory(
+                d, 'TestDirectoryBuilder')
 
-        # REGEX to check if the relevant line contains a git commit
-        self.assertTrue(bool(re.fullmatch(r"g[0-9a-fA-F]{9}",
-                                          info_dict["Commit"])))
-        return
+            # Check that git commit is written
+            info_dict = read_info_dict(
+                os.path.join(path, 'pcpostprocess_info.txt'))
+            self.assertTrue(
+                re.fullmatch(r'g[0-9a-fA-F]{9}', info_dict['Commit']))
 
-    @temp_cwd()
-    def test_directory_with_dev_version(self):
-        with patch("pcpostprocess.directory_builder.__commit_id__", "a000"):
-            test_dir = directory_builder.setup_output_directory("test_output",
-                                                                self.__class__.__name__)
 
-            with open(os.path.join(test_dir, "pcpostprocess_info.txt"), "r") as fin:
-                info_file_contents = [l.strip() for l in fin.readlines()]
-
-            info_dict = make_info_dict(info_file_contents)
-            self.assertEqual(info_dict["Commit"], "a000")
-        return
+if __name__ == '__main__':
+    unittest.main()
 
